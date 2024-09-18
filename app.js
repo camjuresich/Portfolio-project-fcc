2591c4c1447969952fcf2dbaac0ccd1f344ce343
Cameron Juresich <camjuresich@gmail.com>
2024-09-18 02:07:43 -1000

    switched anchor tags to buttons and added event listners to snap scrolling using scrollSnapIntoView

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const menuIcon = document.getElementById("menu-icon");
 const menu = document.getElementById("menu");
 const words = document.querySelectorAll(".word");
 const heroRoles = document.getElementById("hero-roles");
-
+const hero = document.getElementById('hero')
+const projects = document.getElementById('projects')
+const projectOne = document.querySelector('.project-one')
+const projectTwo = document.querySelector('.project-two')
+const projectThree = document.querySelector('.project-three')
+const about = document.getElementById('about')
+const homeLargeBtn = document.getElementById('home-large')
+const projectsLargeBtn = document.getElementById('projects-large')
+const aboutLargeBtn = document.getElementById('about-large')
+const homeSmallBtn = document.getElementById('home-small-btn')
+const projectOneBtn = document.getElementById('project-one-btn')
+const projectTwoBtn = document.getElementById('project-two-btn')
+const projectThreeBtn = document.getElementById('project-three-btn')
+const aboutSmallBtn = document.getElementById('about-btn')
 function cycleRoles() {
     index = 1;
     return () => {
@@ -14,11 +27,30 @@ function cycleRoles() {
     };
 }
 
+function scrollSnaps (section) {
+    document.querySelector('html').style.scrollSnapType = 'none'
+    section.scrollIntoView({behavior: "smooth"})
+    setTimeout(() => {
+        document.querySelector('html').style.scrollSnapType = 'y mandatory'
+    }, 500)
+}
+homeLargeBtn.addEventListener('click', () => scrollSnaps(hero))
+projectsLargeBtn.addEventListener('click', () => scrollSnaps(projects))
+aboutLargeBtn.addEventListener('click', () => scrollSnaps(about))
+homeSmallBtn.addEventListener('click', () => scrollSnaps(hero))
+projectOneBtn.addEventListener('click', () => scrollSnaps(projectOne))
+projectTwoBtn.addEventListener('click', () => scrollSnaps(projectTwo))
+projectThreeBtn.addEventListener('click', () => scrollSnaps(projectThree))
+aboutSmallBtn.addEventListener('click', () => scrollSnaps(about))
 setInterval(cycleRoles(), 1500);
 
 function toggleMenu () {
     menuIcon.classList.toggle("menu-toggled");
     menu.classList.toggle("menu-hidden");
+    // setTimeout(() => {
+        // document.querySelector('html').style.scrollSnapType = 'y mandatory'
+
+    // }, 700)
 }
 
 
